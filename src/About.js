d39169c50b29e2f9b51e4dde2d6bd3e691891575
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-10-06 22:17:07 +0600

    service-trusted

--- src/About.js
+++ src/About.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import HeroSection from './components/HeroSection'
 
 const About = () => {
+
+  const data = {
+    name: "Amazon digital product"
+  }
   return (
-    <div>About</div>
+    <>
+    <HeroSection myData={data}/>
+      
+    </>
   )
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import HeroSection from './components/HeroSection'
+import Services from './components/Services'
+import Trusted from './components/Trusted'
 
 const Home = () => {
+  const data = {
+    name: "Amazon Product"
+  }
   
   return (
     <div>
-      <HeroSection/>
+      <HeroSection myData = {data}/>
+      <Services/>
+      <Trusted/>
     </div>
   )
 }
