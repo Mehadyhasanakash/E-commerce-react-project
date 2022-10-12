7a45b50f5763cf65e375d5e52e5af7d33d9975b9
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-10-12 22:19:33 +0600

    complete-contexAPI

--- src/About.js
+++ src/About.js
@@ -1,16 +1,15 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import HeroSection from './components/HeroSection'
-import { AppContext, useProductContext } from './context/productcontext'
 
 const About = () => {
-  const Mydata = useProductContext();
+  
 
   const data = {
     name: "Amazon digital product"
   }
   return (
     <>
-    {Mydata}
+    
     <HeroSection myData={data}/>
       
     </>
--- src/reducer/productReducer.js
+++ src/reducer/productReducer.js
@@ -1,4 +1,43 @@
 const productReducer = (state, action) =>{
+
+    switch (action.type) {
+        case "SET_LODDING":
+            
+            return{
+                ...state,
+                isLodding: true,
+
+            };
+
+            case "API_ERROR" :
+                return{
+                    ...state,
+                    isLodding: false,
+                    isError: true,
+                };
+
+                case "MY_API_DATA" :
+                    const featureData = action.payload.filter((currentElement) =>{
+                            return currentElement.featured === true;
+                            
+
+                    })
+
+                    return{
+                        ...state,
+                        isLodding: false,
+                        product : action.payload,
+                        futureProduct : featureData,
+                    }
+    
+        default:
+            return{
+                ...state
+            }
+    }
+
+
+
     return state;
 };
 
